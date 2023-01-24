51e53d1f8eae7c553fdd6b7873fa1164a54a6748
alishabab62 <alishabab62@gmail.com>
2023-01-24 20:25:30 +0530

    Complete

--- Admin Pannel/myWorkspace.js
+++
@@ -1,6 +0,0 @@
-
-const bcBtn=document.getElementById('bc-btn');
-bcBtn.addEventListener('click',()=>{
-const demo=document.getElementById("text")
-localStorage.setItem("backgroundColor" ,`${demo.value}`);
-});
